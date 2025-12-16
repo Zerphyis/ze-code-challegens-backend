c58bbeb5b2a94357b358aaba40318d68bcd11b72
Otavio <estudosprogotavio@gmail.com>
2025-12-16 05:34:27 -0300

    commit initial

---
+++ src/main/java/dev/Zerphyis/ZeCodesBackEnd/ZeCodesBackEndApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.ZeCodesBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeCodesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeCodesBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/ZeCodesBackEnd/ZeCodesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.ZeCodesBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeCodesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
