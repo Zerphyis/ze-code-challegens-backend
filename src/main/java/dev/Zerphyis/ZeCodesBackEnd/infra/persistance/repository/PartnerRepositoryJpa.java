ab0436ecb6acb2f5e52176bc2eefd9a6f6a8c12e
Otavio <estudosprogotavio@gmail.com>
2025-12-17 03:58:47 -0300

    TASK06 feat:adicionando interface PartnerRepositoryJpa

---
+++ src/main/java/dev/Zerphyis/ZeCodesBackEnd/infra/persistance/repository/PartnerRepositoryJpa.java
@@ -0,0 +1,26 @@
+package dev.Zerphyis.ZeCodesBackEnd.infra.persistance.repository;
+
+import dev.Zerphyis.ZeCodesBackEnd.infra.persistance.entity.PartnerJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface PartnerRepositoryJpa extends JpaRepository<PartnerJpaEntity, UUID> {
+
+    Optional<PartnerJpaEntity> findByDocument(String document);
+
+    @Query("""
+        SELECT p
+        FROM PartnerJpaEntity p
+        WHERE p.active = true
+          AND within(:point, p.coverageArea) = true
+        ORDER BY distance(p.address, :point)
+        """)
+    Optional<PartnerJpaEntity> searchNearest(
+            @Param("point") org.locationtech.jts.geom.Point point
+    );
+}
