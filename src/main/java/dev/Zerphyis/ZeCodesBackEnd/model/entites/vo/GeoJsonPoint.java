8ff298a600d0a15b29884a3ce4971e44348466ca
Otavio <estudosprogotavio@gmail.com>
2025-12-17 03:55:24 -0300

    TASK04 refactor:corrigindo pacotes de exception em GeoJsonPoint

--- src/main/java/dev/Zerphyis/ZeCodesBackEnd/model/entites/vo/GeoJsonPoint.java
+++ src/main/java/dev/Zerphyis/ZeCodesBackEnd/model/entites/vo/GeoJsonPoint.java
@@ -1,6 +1,6 @@
 package dev.Zerphyis.ZeCodesBackEnd.model.entites.vo;
 
-import dev.Zerphyis.ZeCodesBackEnd.infra.exceptions.InvalidGeoDataException;
+import dev.Zerphyis.ZeCodesBackEnd.model.exceptions.InvalidGeoDataException;
 
 import java.util.Objects;
 
