08009727bd81689fb8eb8ab2d5cb1ddbcfa5d174
Otavio <estudosprogotavio@gmail.com>
2025-12-18 09:31:04 -0300

    TASK11 feat:adicionando useCase ActivePartnerUseCase

---
+++ src/main/java/dev/Zerphyis/ZeCodesBackEnd/model/useCases/ActivePartnerUseCaseImpl.java
@@ -0,0 +1,34 @@
+package dev.Zerphyis.ZeCodesBackEnd.model.useCases;
+
+import dev.Zerphyis.ZeCodesBackEnd.model.entites.Partner;
+import dev.Zerphyis.ZeCodesBackEnd.model.exceptions.PartnerNotFoundException;
+import dev.Zerphyis.ZeCodesBackEnd.model.interfacesCases.ActivePartnerUseCase;
+import dev.Zerphyis.ZeCodesBackEnd.model.repository.PartnerRepositoryGateway;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class ActivePartnerUseCaseImpl implements ActivePartnerUseCase {
+    private final PartnerRepositoryGateway repositoryGateway;
+
+    public ActivePartnerUseCaseImpl(PartnerRepositoryGateway repositoryGateway) {
+        this.repositoryGateway = repositoryGateway;
+    }
+
+    @Override
+    public void execute(UUID id) {
+        Objects.requireNonNull(id, "Id não pode ser nulo");
+
+        Partner partner = repositoryGateway
+                .findById(id)
+                .orElseThrow(() ->
+                        new PartnerNotFoundException(
+                                "Parceiro não encontrado para desativação: " + id
+                        )
+                );
+
+        partner.isActive();
+
+        repositoryGateway.save(partner);
+    }
+}
