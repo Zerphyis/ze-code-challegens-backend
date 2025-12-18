069279a52f7f4fccbcc2427bcbd2829d1620ab0a
Otavio <estudosprogotavio@gmail.com>
2025-12-18 09:25:47 -0300

    TASK09 feat:adicionando interface GetAllPartnerUseCase

---
+++ src/main/java/dev/Zerphyis/ZeCodesBackEnd/model/interfacesCases/GetAllPartnerUseCase.java
@@ -0,0 +1,9 @@
+package dev.Zerphyis.ZeCodesBackEnd.model.interfacesCases;
+
+import dev.Zerphyis.ZeCodesBackEnd.model.entites.Partner;
+
+import java.util.List;
+
+public interface GetAllPartnerUseCase {
+    List<Partner> execute();
+}
