ab8adfad8f18d5618bde92dc895ec5c404aadb19
Otavio <estudosprogotavio@gmail.com>
2025-12-26 02:05:33 -0300

    TASK15 feat:extendendo de DomainException em PartnerNotFoundException

--- src/main/java/dev/Zerphyis/ZeCodesBackEnd/model/exceptions/PartnerNotFoundException.java
+++ src/main/java/dev/Zerphyis/ZeCodesBackEnd/model/exceptions/PartnerNotFoundException.java
@@ -1,6 +1,6 @@
 package dev.Zerphyis.ZeCodesBackEnd.model.exceptions;
 
-public class PartnerNotFoundException extends RuntimeException {
+public class PartnerNotFoundException extends DomainException {
     public PartnerNotFoundException(String message) {
         super(message);
     }
